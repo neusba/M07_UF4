72bdd4731124e541135089c851a33df633de0c58
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-03-10 23:39:27 +0100

    Archivos practica

---
+++ neus_env/TIC_BCN_nba/centre/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('teachers', views.index_profes, name='index_profes'),
+    path('students', views.index_alumnat, name='index_alumnat')
+]
